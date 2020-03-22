a3da2daad9829064e6a8808a8034d539bb29d306
Jens Lindström <02jli01@skola.falun.se>
2020-03-22 22:21:27 +0100

    Implemented folder router

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var path = require('path');
+
+const folder_router = (dir = './routes') => {
+    if (!fs.existsSync(dir)) {
+        throw Error('Route folder does not exist!');
+    } else {
+        let files = GetFiles(dir).map(file => './' + file.replace(/\\/g, "/"));
+        let routes = [];
+        files.forEach(file => {
+            routes.push(require(file));
+        });
+
+        return routes;
+    }
+}
+
+const GetFiles = dir =>
+    fs.readdirSync(dir).reduce((files, file) => {
+        const name = path.join(dir, file);
+        const isDirectory = fs.statSync(name).isDirectory();
+        return isDirectory ? [...files, ...GetFiles(name)] : [...files, name];
+    }, []);
+
+module.exports = folder_router;
